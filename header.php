41dad5d40ae64dc6a01ea82e862bdae0cff5072a
Butters <k.butterfield08@gmail.com>
2020-10-20 23:16:01 -0500

    Pushed from PC
    Going to call it a night, but if I work on it some more I will work on it from laptop

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ICS325 - Meal Planner</title>
+        <meta charset='utf-8'/>
+        <link rel='stylesheet' href='Style/style.css'>
+        <link rel='stylesheet' href='Style/index.css'>
+    </head>
+    <body>
+        <header>
+            <img Style='width: 100%; height: 300px;'src='Images/kitchen.jpg' alt=''>
+        </header>
+        <nav>
+            <ul>
+                <li><a href='index.php'>Home</a></li>
+                <li><a href='#cookbook'>Recipes</a></li>
+            </ul>
+        </nav>";
+
+?>
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "header.php"; ?>
+
+<main>
+    <table>
+        <tbody>
+            <tr>
+                <td>Row 1 - Cell 1</td>
+                <td>Row 1 - Cell 2</td>
+                <td>Row 1 - Cell 3</td>
+            </tr>
+            <tr>
+                <td>Row 2 - Cell 1</td>
+                <td>Row 2 - Cell 2</td>
+                <td>Row 2 - Cell 3</td>
+            </tr>
+        </tbody>
+    </table>
+</main>
+
+<?php include "footer.php"; ?>
