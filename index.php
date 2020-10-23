514f99ecb4dc4b2a9c83b4e262ea569db5daeacb
Butters <k.butterfield08@gmail.com>
2020-10-23 09:44:16 -0500

    Updated main
    Updated main

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
 <?php include "header.php"; ?>
 
+<?php 
+
+
+
+    $foodName1 = "Banana Break";
+    $foodName2 = "Deer Meat Spicy Jerky";
+    $foodName3 = "Spagethi";
+    $foodName4 = "That One thing I can't think of";
+    $foodName5 = "Quick and Easy";
+    $foodName6 = "Names are super confusing!";
+?>
+
 <main>
-    <table>
+    <table id="menu">
         <tbody>
             <tr>
-                <td>Row 1 - Cell 1</td>
-                <td>Row 1 - Cell 2</td>
-                <td>Row 1 - Cell 3</td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName1; ?></td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName2; ?></td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName3; ?></td>
             </tr>
             <tr>
-                <td>Row 2 - Cell 1</td>
-                <td>Row 2 - Cell 2</td>
-                <td>Row 2 - Cell 3</td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName4; ?></td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName5; ?></td>
+                <td><img class="foodIcon" src="Images/emptyIcon.png" alt=""><br><?php  echo $foodName6; ?></td>
             </tr>
         </tbody>
     </table>
+    <div id="howToMakeBox">
+        this should be working!
+    </div>
 </main>
 
 <?php include "footer.php"; ?>
