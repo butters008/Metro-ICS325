eda42c55c0bbcb5460fdc302eb883623ffe3c1f9
Sam Jones <sam720_01@yahoo.com>
2020-11-02 23:16:57 -0600

    Tweaks to match naming

--- modifyRecipe.php
+++ modifyRecipe.php
@@ -1,13 +1,11 @@
 <?php include "header.php"; ?>
-
+<!-- TODO implement $_POST data in order to pull existing recipe details -->
 <main>
     <!-- Making a switch here to choose from modifing or adding -->
     <br /><br />
     <section class="submitForm">
         <h3 id="submitTitle">Cookbook Recipe</h3><br />
         <form action="" method="post">  
-            <!--(Thought process for menu decission was from Applebees) 
-                Make them choose: dinner -  dessert/snack -  appatizer -->
             
             <!-- Then comes basic information for recipe
                 name, description, assigned 1 of three icons that user previous choosen -->
@@ -26,8 +24,14 @@
             <table id="ingredientTable">
                 <thead>
                     <tr>
-                        <th>Ingredent Name: </th>
-                        <th>Amount: </th>
+                        <th>Qty: </th>
+                        <th>Units:</th>
+                        <th>Ingredient Name:</th>
+                    </tr>
+                    <tr>
+                        <td><input type='number' name='ingredientQty[]'></td>
+                        <td><select name='qty' id='qty'><option value='Qty'><option value='Unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='audi'>oz</option></select></td>
+                        <td><input type='text' name='ingredientName[]'></td>
                     </tr>
                 </thead>
                 <tbody id="tbody"></tbody>
@@ -38,7 +42,8 @@
             <!-- This is where we tell them to make the instructions as to how to make the recipe
             Thinking that this will output to a text file, than we store this locally on the page 
             For now localhost/server, but once we get further developed, we store on website server            
-            REASON: We take the textfile and load onto instruction on main page with correct formatting (I believe) -->
+            REASON: We take the textfile and load onto instruction on main page with correct formatting (I believe) - Keith-->
+            <!-- We can just store it in a string with $_POST data but we need to account for sql injection and make sure we have form validation -->
             <label for="ingredientInstuction"><strong>Instructions for Recipe:</strong></label><br/>
             <textarea id="ingredientInstuction" name="ingredientInstuction" rows="15" cols="50"></textarea>
             <p><input type="submit" value="submit" name="submit"></p><br/><br/>
@@ -50,11 +55,10 @@
 <script>
     function addItem(){
         var html = "<tr>";
-            html += "<td><input name='ingredentName[]'></td>";
-            html += "<td><input name='ingredentAmount[]'></td>";
-            html += "<td><select name='amount' id='amount'><option value='Amount'><option value='unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='audi'>oz</option></select></td>";
+            html += "<td><input type='number' name='ingredientQty[]'></td>";
+            html += "<td><select name='qty' id='qty'><option value='qty'><option value='unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='audi'>oz</option></select></td>";
+            html += "<td><input type='text' name='ingredientName[]'></td>";
             html += "</tr>";
-
             document.getElementById("tbody").insertRow().innerHTML = html;
     }
 </script>
