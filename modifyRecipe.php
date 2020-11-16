2a3059924abb410d38a930e5fb69f60c7be380c5
Butters <k.butterfield08@gmail.com>
2020-11-16 17:18:13 -0600

    Update modifyRecipe.php

--- modifyRecipe.php
+++ modifyRecipe.php
@@ -1,4 +1,39 @@
-<?php include "header.php"; ?>
+<?php 
+
+    include "header.php"; 
+    include "dbCred.php";
+
+    if(isset($_POST["submit"])){
+
+        //Declaring and assigning variables from form
+        $recipeName = $_POST["recipeName"];
+        $cookTime = $_POST["cookTime"];
+        $recipeInstuction = $_POST["recipeInstuction"];
+
+        //testing - success assigning from form to variables
+        echo "$recipeName<br>";
+        echo "$cookTime<br>";
+        echo "$recipeInstuction<br>";
+
+        
+        $sql = "INSERT INTO recipe (recipe_name, cook_time, recipe_instructions) VALUES ($recipeName, $cookTime, $recipeInstuction);"; 
+        // mysqli_query($link, $sql);
+        
+        // Prepare the statement
+        $stmt = $link->prepare($sql);
+        // Bind the parameters
+        $stmt->bind_param('sss', $recipeName, $cookTime, $recipeInstuction);
+        //Execute the statement
+        $stmt->execute();
+
+        // Bind the results - I dont think I need to use Bind....
+        // $stmt->bind_result($rID,$rName,$rCookTime,$rInstructions,$rImage, $iID,$iName,$iMeasureType, $iQty);
+
+        echo "<p>Recipe Added to database</p>";
+
+    }
+
+?>
 <!-- TODO implement $_POST data in order to pull existing recipe details -->
 <!-- I added an auto increment to the id column (recipe and shopping list) on the database so we don't have to worry about creating a new ID for each added recipe-->
 <main>
@@ -6,18 +41,13 @@
     <br /><br />
     <section class="submitForm">
         <h3 id="submitTitle">Cookbook Recipe</h3><br />
-        <form action="" method="post">  
+        <form action="modifyRecipe.php" method="post">  
             
-            <!-- Then comes basic information for recipe
-                name, description, assigned 1 of three icons that user previous choosen -->
             <label for="recipeName">Name of Recipe:</label><br>
-            <input type="text" id="recipeName" name="recipeName"><br><br>
+            <input type="text" id="recipeName" name="recipeName"><br><br> <!-- recipe_name -->
 
             <label for="cookTime">Cooking Time for Recipe:</label><br>
-            <input type="text" id="cookTime" name="cookTime"><br><br>
-
-            <label for="recipeDescription">Description of Recipe:</label><br>
-            <input type="text" id="recipeDescription" name="recipeDescription"><br><br>
+            <input type="text" id="cookTime" name="cookTime"><br><br> <!-- cook_time -->
 
             <!-- Adding ingredents to recipe, making this an array or table 
             Example taken from: https://www.youtube.com/watch?v=qYbkQpSI9_o -->
@@ -31,22 +61,17 @@
                     </tr>
                     <tr>
                         <td><input type='number' name='ingredientQty[]'></td>
-                        <td><select name='qty' id='qty'><option value='Qty'><option value='Unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='audi'>oz</option></select></td>
+                        <td><select name='qty' id='qty'><option value='Qty'><option value='Unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='oz'>oz</option></select></td>
                         <td><input type='text' name='ingredientName[]'></td>
                     </tr>
                 </thead>
                 <tbody id="tbody"></tbody>
-            </table>
+            </table><br>
+            <p><button type="button" onclick="addItem();">Add Item</button></p><br><br>
             
-            <p><button type="button" onclick="addItem();">Add Item</button></p>
-            <br><br>
-            <!-- This is where we tell them to make the instructions as to how to make the recipe
-            Thinking that this will output to a text file, than we store this locally on the page 
-            For now localhost/server, but once we get further developed, we store on website server            
-            REASON: We take the textfile and load onto instruction on main page with correct formatting (I believe) - Keith-->
-            <!-- We can just store it in a string with $_POST data but we need to account for sql injection and make sure we have form validation -->
-            <label for="ingredientInstuction"><strong>Instructions for Recipe:</strong></label><br/>
-            <textarea id="ingredientInstuction" name="ingredientInstuction" rows="15" cols="50"></textarea>
+            <!-- recipe_instructions -->
+            <label for="recipeInstuction"><strong>Instructions for Recipe:</strong></label><br/><br/>
+            <textarea id="recipeInstuction" name="recipeInstuction" rows="15" cols="50"></textarea><br/><br/>  
             <p><input type="submit" value="submit" name="submit"></p><br/><br/>
         </form>
     </section>
@@ -57,7 +82,7 @@
     function addItem(){
         var html = "<tr>";
             html += "<td><input type='number' name='ingredientQty[]'></td>";
-            html += "<td><select name='qty' id='qty'><option value='qty'><option value='unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='audi'>oz</option></select></td>";
+            html += "<td><select name='qty' id='qty'><option value='qty'><option value='unit'>Unit</option><option value='cup'>Cup</option><option value='tbsp'>Tbsp</option><option value='tsp'>Tsp</option><option value='oz'>oz</option></select></td>";
             html += "<td><input type='text' name='ingredientName[]'></td>";
             html += "</tr>";
             document.getElementById("tbody").insertRow().innerHTML = html;
